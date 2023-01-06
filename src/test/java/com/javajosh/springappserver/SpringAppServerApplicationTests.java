cd1d1ee3c997d042deab6317441fccac2eb0c7df
JavaJosh <github@javajosh.com>
2023-01-06 01:45:35 -0500

    Remove some dependencies and fix a test.

--- src/test/java/com/javajosh/springappserver/SpringAppServerApplicationTests.java
+++ src/test/java/com/javajosh/springappserver/SpringAppServerApplicationTests.java
@@ -1,13 +1,16 @@
 package com.javajosh.springappserver;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class SpringAppServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
+//		Assertions.fail();
 	}
-
 }
